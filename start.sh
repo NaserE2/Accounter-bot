6d92b822913fab67bc33fd87b3eccfbaedc1a4f4
Naser Ebrahimzadeh <naserebrahimzade@gmail.com>
2026-07-26 02:14:38 +0330

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# اجرای Streamlit در پس‌زمینه تا پورت 8501 باز شود و Render سرویس را زنده نگه دارد
-streamlit run dashboard.py --server.port=8501 --server.address=0.0.0.0 &
-
-# اجرای ربات تلگرام در پیش‌زمینه
-python bot.py
